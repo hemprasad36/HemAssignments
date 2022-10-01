1095e695acaf18a0bb92b34056aa0ac14bb9bf17
hemprasad36 <114832649+hemprasad36@users.noreply.github.com>
2022-10-02 02:52:35 +0530

    DAY1

---
+++ day1/Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+	
+	byte carrank = '1';
+	int carcolor = 3116;
+	short mileaege = 14; 
+	long cost = 444000l;
+	float totalkms = 55441.54f;
+	double expectedkms = 349000.333633;
+	char initials = 'h';
+	String carbrand = "WagonR";
+	// TODO Auto-generated method stub
+
+}
